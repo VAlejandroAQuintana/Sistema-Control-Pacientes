92a6d45ea99d465e68aec8d89a63115aed432b29
alemr214 <ale.mr0214@gmail.com>
2024-04-01 21:46:01 -0600

    adding .gitignore

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.itver.sistemacontrolpacientes {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.itver.sistemacontrolpacientes to javafx.fxml;
+    exports com.itver.sistemacontrolpacientes;
+}
